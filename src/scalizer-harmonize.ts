24808ff0b503d519745ce003af34a27f6dc4a4ca
Chris Feijoo <christophe@feijoo.fr>
2016-11-27 20:23:18 +0100

    Working version of Scalizer Harmonize

--- src/scalizer-harmonize.ts
+++ src/scalizer-harmonize.ts
@@ -3,28 +3,52 @@
 import * as command from 'commander'
 import { writeFileSync } from 'fs'
 import getYaml from './getYaml'
-import { Scale } from './typings/Scale'
-
-const getTotalPoints = (scale: Scale) => {
-
-}
-
-const harmonizeScale = (scale: {}) => {
-
-}
+import { Scale, Skill } from './typings/Scale'
+import { safeDump } from 'js-yaml'
+import { set } from 'monolite'
 
 command
   .option(
   '-o, --output <dir>',
   'save harmonized scale to a file'
   )
-  .action((scaleFile, options) => {
-    const scale = getYaml(scaleFile)
-    const harmonizedScale = harmonizeScale(scale)
-
-    if (options.output)
-      writeFileSync(options.output, harmonizedScale, { encoding: 'utf8' })
-    else
-      console.log(scale)
-  })
   .parse(process.argv)
+
+const getSkillPoints =
+  (scale: Scale, skill: Skill) =>
+    scale.sections
+      .map(section =>
+        section.questions
+          .map(question =>
+            question.skills[skill] || 0
+          )
+          .reduce((a, b) => a + b)
+      )
+      .reduce((a, b) => a + b)
+
+const updateSkillPoints =
+  (scale: Scale, skill: Skill, coefficient: number): Scale =>
+    set(scale, _ => _.sections)(sections =>
+      sections.map(section =>
+        set(section, _ => _.questions)(questions =>
+          questions.map(question =>
+            set(question, _ => _.skills[skill])(
+              ((question.skills[skill] || 0) * coefficient) | 0
+            )
+          )
+        )
+      )
+    )
+
+const harmonizeScale = (scale: Scale) =>
+  scale.skills.reduce((scale, skill) =>
+    updateSkillPoints(scale, skill, 100 / getSkillPoints(scale, skill))
+  , scale)
+
+const scale = getYaml(command.args[0])
+const harmonizedScale = safeDump(harmonizeScale(scale))
+
+if (command.options.output)
+  writeFileSync(command.options.output, harmonizedScale, { encoding: 'utf8' })
+else
+  console.log(harmonizedScale)
