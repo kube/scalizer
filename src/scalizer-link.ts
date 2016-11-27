3d5ca23f940f98bcd4c1713f07a2672d434ccbd4
Chris Feijoo <christophe@feijoo.fr>
2016-11-27 23:30:19 +0100

    Add --watch flag to Link subcommand

--- src/scalizer-link.ts
+++ src/scalizer-link.ts
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 import * as commander from 'commander'
-import { writeFileSync } from 'fs'
+import { writeFileSync, watchFile } from 'fs'
 import { safeDump } from 'js-yaml'
 
 import getYaml from './lib/getYaml'
@@ -13,6 +13,7 @@ const command = commander as {
   output?: string
   harmonize?: boolean
   format?: boolean
+  watch?: boolean
 } & typeof commander
 
 command
@@ -28,22 +29,37 @@ command
   '-f, --format',
   'format scale for 42 intra'
   )
+  .option(
+  '-w, --watch',
+  'watch files and update on change'
+  )
   .parse(process.argv)
 
 const [headerFiles, ...sectionsFiles] = command.args.slice(0, -1)
-const header = getYaml(headerFiles)
-const sections = sectionsFiles ? sectionsFiles.map(getYaml) : []
 
-// Link scale and apply desired transformations
-let scale: any = linkScale(header, sections)
-scale = command.harmonize ? harmonizeScale(scale) : scale
-scale = command.format ? formatScale(scale) : scale
+function task () {
+  const header = getYaml(headerFiles)
+  const sections = sectionsFiles ? sectionsFiles.map(getYaml) : []
 
-// Convert scale to YAML string for output
-const output = safeDump(scale)
+  // Link scale and apply desired transformations
+  let scale: any = linkScale(header, sections)
+  scale = command.harmonize ? harmonizeScale(scale) : scale
+  scale = command.format ? formatScale(scale) : scale
 
-// Output or save scale depending on options
-if (command.output)
-  writeFileSync(command.output, output, { encoding: 'utf8' })
-else
-  console.log(output)
+  // Convert scale to YAML string for output
+  const output = safeDump(scale)
+
+  // Output or save scale depending on options
+  if (command.output)
+    writeFileSync(command.output, output, { encoding: 'utf8' })
+  else
+    console.log(output)
+}
+
+// Watch change events if requested
+if (command.watch)
+  [headerFiles, ...sectionsFiles].forEach(file =>
+    watchFile(file, task)
+  )
+
+task()
