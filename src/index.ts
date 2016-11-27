f9c0a2a31a5de1f1eefa7d46de67920906167d71
Chris Feijoo <christophe@feijoo.fr>
2016-11-27 19:34:43 +0100

    index.ts: link command requires sections

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import * as program from 'commander'
 
 program
   .version(packageInfo.version)
-  .command('link <header> [sections...]', 'Link header and sections')
+  .command('link <header> <sections...>', 'Link header and sections')
   .command('harmonize <file>', 'Harmonize points of sections')
   .command('format <file>', 'Format to 42 intra scale format')
   .parse(process.argv)
