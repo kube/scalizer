ee34a0ac996f31982aa343be3db4804afa8e068e
Chris Feijoo <christophe@feijoo.fr>
2016-11-28 17:27:20 +0100

    Tweak syntax lib/harmonize

--- src/lib/harmonize.ts
+++ src/lib/harmonize.ts
@@ -13,15 +13,11 @@ import { Scale, Skill } from '../typings/ScalizerScale'
 
 const getSkillPoints =
   (scale: Scale, skill: Skill) =>
-    scale.sections
-      .map(section =>
-        section.questions
-          .map(question =>
-            question.skills[skill] || 0
-          )
-          .reduce((a, b) => a + b)
-      )
-      .reduce((a, b) => a + b)
+    scale.sections.map(section =>
+      section.questions.map(question =>
+        question.skills[skill] || 0
+      ).reduce((a, b) => a + b)
+    ).reduce((a, b) => a + b)
 
 const updateSkillPoints =
   (scale: Scale, skill: Skill, coefficient: number): Scale =>
